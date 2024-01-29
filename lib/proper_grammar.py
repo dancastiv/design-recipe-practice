193693783222b08d31123be5df903f148247c682
Danny Castilla <dancastiv@gmail.com>
2024-01-29 14:09:36 +0000

    fixed acceptable punctuation

--- lib/proper_grammar.py
+++ lib/proper_grammar.py
@@ -1,5 +1,4 @@
-import string
 def check_grammar(text):
     if text == '':
         raise Exception('Please provide text to analyse.')
-    return text[0].isupper() and text[-1] in string.punctuation
+    return text[0].isupper() and text[-1] in '.?!'
