c226ccc6f61d8a0de06dd08997c4f3dd09df8c53
Danny Castilla <dancastiv@gmail.com>
2024-02-01 18:06:27 +0000

    commit before I rewrite TodoList class

--- lib/todo_list.py
+++ lib/todo_list.py
@@ -5,6 +5,24 @@ class TodoList:
     def add(self, task):
         self.todo_list.append(task)
 
+    def incomplete(self):
+        # Returns:
+        #   A list of Todo instances representing the todos that are not complete
+        pass
+
+    def complete(self):
+        # Returns:
+        #   A list of Todo instances representing the todos that are complete
+        pass
+
+    def give_up(self):
+        # Returns:
+        #   Nothing
+        # Side-effects:
+        #   Marks all todos as complete
+        pass
+
+# outdated
     def show_list(self):
         if self.todo_list == []:
             raise Exception('To-do list is empty. Please provide tasks.')
