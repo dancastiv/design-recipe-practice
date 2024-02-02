4e7101c5b236bb9f447a44048af063e04ca9f5c6
Danny Castilla <dancastiv@gmail.com>
2024-02-02 11:34:59 +0000

    forgot to delete a section lol

--- lib/todo_list.py
+++ lib/todo_list.py
@@ -16,15 +16,3 @@ class TodoList:
     def give_up(self):
         [task.mark_complete() for task in self.todo_list]
         self.complete()
-
-# outdated
-    def show_list(self):
-        if self.todo_list == []:
-            raise Exception('To-do list is empty. Please provide tasks.')
-        return self.todo_list
-
-    def mark_completed(self, task):
-        if task not in self.todo_list:
-            raise Exception('Exception: Task not found. Please provide task to mark as completed.')
-        self.todo_list.remove(task)
-        return f'Task marked as completed. Updated list: {self.todo_list}'
